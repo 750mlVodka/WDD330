aba7ab69d1ca130844ded4f0471b6926094068ea
yamsir <yamsirgg@gmail.com>
2025-11-11 15:41:41 -0600

    refactor: update product details rendering and event handling for improved clarity and functionality

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -9,9 +9,9 @@ export default class ProductDetails {
     }
 
     async init() {
-        this.product = await this.dataSource.getProductById(this.productId);
+        this.product = await this.dataSource.findProductById(this.productId);
         this.renderProductDetails();
-        document.getElementById("addToCart").addEventListener("click", this.addToCart.bind(this))
+        document.getElementById("addToCart").addEventListener("click", this.addProductToCart.bind(this))
     }
 
     addProductToCart() {
@@ -26,17 +26,17 @@ export default class ProductDetails {
 }
 
 function productDetailsTemplate(product) {
-    document.querySelector("h2").textContent = product.Brand.Name;
-    document.querySelector("h3").textContent = product.NameWithoutBrand;
+    document.querySelector("h3").textContent = product.Brand.Name;
+    document.querySelector("h2").textContent = product.NameWithoutBrand;
 
     const productImage = document.getElementById("productImage");
     productImage.src = product.Image;
     productImage.alt = product.NameWithoutBrand;
 
-    document.getElementById("productPrice").textContent = product.FinalPrice;
+    document.getElementById("productPrice").textContent = `$${product.FinalPrice}`;
     document.getElementById("productColor").textContent = product.Colors[0].ColorName;
     document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
-    
+
     document.getElementById("addToCart").dataset.id = product.Id;
 }
 
--- src/js/product.js
+++ src/js/product.js
@@ -3,8 +3,8 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
-const productId = getParam(product, dataSource);
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
