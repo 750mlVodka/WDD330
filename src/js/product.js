74890fb8fc85f7306f1d8f07bfd482f83d20c76d
yamsir <yamsirgg@gmail.com>
2025-11-11 14:48:42 -0600

    refactor: update product details rendering to use DOM manipulation instead of template literals

--- src/js/product.js
+++ src/js/product.js
@@ -4,9 +4,9 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam(product, dataSource)
+const productId = getParam(product, dataSource);
 
-const product = new ProductDetails(productId, dataSource)
+const product = new ProductDetails(productId, dataSource);
 product.init();
 
 // function addProductToCart(product) {
